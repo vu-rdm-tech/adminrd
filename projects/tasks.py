a2eef0fe54ce965e472c3811b077479c65fe0837
peer35 <p.j.m.vos@vu.nl>
2023-11-07 11:47:54 +0100

    new owner bug

--- projects/tasks.py
+++ projects/tasks.py
@@ -23,7 +23,11 @@ def _store_fa_data(fa, collected):
     owner.email = fa['owner']['email']
     owner.backend = fa['owner']['backend']['value']
     owner.status = fa['owner']['status']
-    owner.last_login = datetime.strptime( fa['owner']['last_login'], '%Y-%m-%d %H:%M:%S').astimezone(tz)
+    try {
+        owner.last_login = datetime.strptime( fa['owner']['last_login'], '%Y-%m-%d %H:%M:%S').astimezone(tz)
+    } except {
+        owner.last_login = None
+    }	
     owner.save()
     
     #department
